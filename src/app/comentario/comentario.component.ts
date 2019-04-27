80cc843bbab8f418ac6fab5e9f2f3a27741b2997
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-04-27 10:55:31 -0300

    sync

--- src/app/comentario/comentario.component.ts
+++ src/app/comentario/comentario.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComentarioComponent implements OnInit {
 
+  comments = [{}, {}]
+
   constructor() { }
 
   ngOnInit() {
--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideosComponent implements OnInit {
 
+  videos = [{}, {}]
+
   constructor() { }
 
   ngOnInit() {
