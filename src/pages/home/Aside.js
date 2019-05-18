86917b241bcc9e3a07b4713a8a992f515d93167d
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-18 16:34:52 -0300

    init Aside

---
+++ src/pages/home/Aside.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Aside extends React.Component{
+  render(){
+    return(
+      <aside className="col-md-2">
+        Home Aside
+      </aside>
+    )
+  }
+}
