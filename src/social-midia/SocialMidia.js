0793255a91c6a8f974cfb7dd19c73e9d078a2cce
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-18 11:02:53 -0300

    created social midia

--- src/social-midia/SocialMidia.js
+++ src/social-midia/SocialMidia.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import './SocialMidia.sass';
+import facebook from './facebook.svg';
+import instagram from './instagram.svg';
+import whatsapp from './whatsapp.svg';
+import youtube from './youtube.svg';
 
-export default class SocialMidia extends React.Component{
+
+class NavItem extends React.Component{
+
+
   render(){
     return(
-      <nav className="navbar ml-1">
-        SocialMidia Here
-      </nav>
+      <a href={this.props.href} target="_blank" className="mr">
+        <img
+          src={this.props.icon}
+          className="social-icon"
+          alt={this.props.text} />
+      </a>
     )
   }
+
+
+}
+
+
+export default class SocialMidia extends React.Component{
+
+
+  render(){
+
+    const links = [];
+
+    const social_links = [
+      {href: 'https://www.facebook.com/TurismoLu/', text: 'facebook site', icon: facebook},
+      {href: '#', text: 'facebook site', icon: instagram},
+      {href: '#', text: 'facebook site', icon: youtube},
+      {href: 'https://wa.me/5516991250303', text: 'facebook site', icon: whatsapp}
+    ]
+
+    social_links.forEach((link)=>{
+      links.push(<NavItem href={link.href} text={link.text} icon={link.icon}/>)
+    });
+
+    return(
+      <div className="navbar-nav ml-1">{links}</div>
+    )
+  }
+
+
 }
