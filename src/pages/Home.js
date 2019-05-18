32bc8cb1beae5f91e6eb1869c80e33597485fa67
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-18 16:34:05 -0300

    add Article and Side to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Footer from '../menu/Footer';
 import Header from '../menu/Header';
+import Article from './home/Article';
+import Aside from './home/Aside';
+import './home/Home.sass';
 
 function Home() {
   return (
     <div className="App container">
       <Header />
+      <div className="row pt15">
+        <Article />
+        <Aside/>
+      </div>
       <Footer />
     </div>
   );
