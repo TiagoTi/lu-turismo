109bf2aaee31ad7e81add23ffc2bdf52ca5a97cb
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-25 20:01:29 -0300

    create a place card class

---
+++ src/components/card/PlaceCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import regiao_dos_lagos from './regiao_dos_lagos.jpg';
+
+export default class PlaceCard extends React.Component{
+    render(){
+      return(
+        <div className="card card-border col-md-3  mglf">
+          <img className="card-img-top car-image" src={regiao_dos_lagos} alt="Card image cap"/>
+          <div className="card-body d-flex justify-content-center">
+            <h4 className="card-title c-blue-palle">Cabo Frio região dos lagos</h4>
+            <div className="aaa">
+              <h5 className="card-title c-red font-weight-bold">A partir</h5>
+              <p className="card-text">10x<br/>R$ 125,00</p>
+              <a href="www.google.com.br" className="btn btn-primary">detalhes</a>
+            </div>
+  
+          </div>
+        </div>
+      )
+    }
+  }
