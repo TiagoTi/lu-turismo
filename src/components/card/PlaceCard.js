40604580f1b6dafa17fcb28fb17119f440ae4e6b
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-05-31 22:38:17 -0300

    implements Place Card

--- src/components/card/PlaceCard.js
+++ src/components/card/PlaceCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import regiao_dos_lagos from './regiao_dos_lagos.jpg';
+import './PlaceCard.sass';
 
 export default class PlaceCard extends React.Component{
     render(){
@@ -9,7 +10,7 @@ export default class PlaceCard extends React.Component{
           <div className="card-body d-flex justify-content-center">
             <h4 className="card-title c-blue-palle">Cabo Frio região dos lagos</h4>
             <div className="aaa">
-              <h5 className="card-title c-red font-weight-bold">A partir</h5>
+              <h5 className="card-title c-red font-weight-bold price-label">A partir</h5>
               <p className="card-text">10x<br/>R$ 125,00</p>
               <a href="www.google.com.br" className="btn btn-primary">detalhes</a>
             </div>
