8d385bcbd91d69b74b63118cbcf99a25922ea6fa
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-14 18:32:09 -0300

    configura card

--- src/components/card/PlaceCard.js
+++ src/components/card/PlaceCard.js
@@ -7,14 +7,22 @@ export default class PlaceCard extends React.Component{
       return(
         <div className="card card-border col-md-3  mglf">
           <img className="card-img-top car-image" src={regiao_dos_lagos} alt="Card image cap"/>
-          <div className="card-body d-flex justify-content-center">
-            <h4 className="card-title c-blue-palle the-place-title">Cabo Frio região dos lagos</h4>
-            <div className="aaa">
-              <h5 className="card-title c-red font-weight-bold price-label">A partir</h5>
-              <p className="card-text">10x<br/>R$ 125,00</p>
-              <a href="www.google.com.br" className="btn-primary btn-sm">detalhes</a>
+          <div className="card-body">
+
+            <div className="justify-content-flex-end">
+              <div className="label-price">
+                <p className="c-red font-weight-bold">A partir</p>
+              </div>
             </div>
-  
+
+            <div className="justify-content-flex-end">
+              <p className="a">10x R$ 125,00</p>
+            </div>
+
+            <div className="">
+                <p className="a"></p>
+            </div>
+            
           </div>
         </div>
       )
