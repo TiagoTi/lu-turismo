fd0103a8c8b6654c6929e7b5ccdab052c1e512f4
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-14 19:10:29 -0300

    adiciona alguns videos na lateral

--- src/pages/home/Aside.js
+++ src/pages/home/Aside.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import './Home.sass';
+import './Video.sass';
 
 export default class Aside extends React.Component{
   render(){
     return(
-      <aside className="col-md-2">
-        Home Aside
+      <aside className="col-md-3">
+        <div>
+          <Video source="https://www.youtube.com/embed/L0cWBjfynrs"/>
+          <Video source="https://www.youtube.com/embed/AF_UBXCA5-I"/>
+          <Video source="https://www.youtube.com/embed/SEzx-nTO0Yg"/>
+        </div>
       </aside>
+    )
+  }
+}
+
+
+class Video extends React.Component{
+  render(){
+    return(
+      <div className='video'>
+        <iframe src={this.props.source} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </div>
     )
   }
 }
