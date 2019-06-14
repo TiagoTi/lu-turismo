82b72af534f14fbcbd49979f6221944ce69261a9
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-14 19:10:12 -0300

    altera a largura da linha dos cards da main

--- src/pages/home/Article.js
+++ src/pages/home/Article.js
@@ -9,7 +9,7 @@ import PlaceCard from '../../components/card/PlaceCard';
 export default class Article extends React.Component{
   render(){
     return(
-      <article className="col-md-10 row">
+      <article className="col-md-9 row all-cards">
         <PlaceCard />
         <PlaceCard />
         <PlaceCard />
