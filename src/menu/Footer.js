742c71ecaa2188dc2b3bd7aa3f152f3c120d7736
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-16 13:49:30 -0300

    sync

--- src/menu/Footer.js
+++ src/menu/Footer.js
@@ -5,26 +5,45 @@ import './Footer.sass';
 function Footer() {
   return (
     <footer className="footer">
-        <p className="you-can">Você pode conhecer o mundo!</p>
-        <div className="contact-us">
-          <div className="row">
-            <div className="col-4">
-              <p>oi</p>
-            </div>
-            <div className="col-8">
-            <form>
-            <div className="row">
-              <div className="col-6"><input type="text" className="custom" />
-            </div>
-              <div className="col-6">
-              <textarea className="custom"></textarea>
-              </div>
-            </div>
-          </form>
-            </div>
+      <p className="you-can">Você pode conhecer o mundo!</p>
+      <div className="contact-us">
+        <div className="row">
+          <div className="col-4">
+            <p className="address">
+              Rua General Telles, 1041<br/>
+              Centro - Franca-SP<br/>
+              CEP: 14405-090
+            </p>
           </div>
-          
+          <div className="col-8">
+            <form>
+              <div className="row">
+                <div className="col-6">
+                  <div className="form-group">
+                    <input type="text" className="custom form-control" placeholder="Nome"  />
+                  </div>
+                  <div className="form-group">
+                    <input type="email" className="custom form-control" placeholder="Email"  />
+                  </div>
+                  <div className="form-group">
+                    <input type="text" className="custom form-control" placeholder="Titulo"  />
+                  </div>
+                </div>
+                <div className="col-6">
+                <div className="form-group">
+                    <textarea className="custom form-control" rows="5" placeholder="Mensagem"></textarea>
+                  </div>
+                  <div className="form-group">
+                    <input type="submit" className="btn btn-primary" placeholder="Titulo"  />
+                  </div>
+                  
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
+
+      </div>
     </footer>
   );
 }
