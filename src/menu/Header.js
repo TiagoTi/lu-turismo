197caffb4c62719ce4ab302b0f4c653e9d09101c
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-18 08:55:33 -0300

    fixed menu

--- src/menu/Header.js
+++ src/menu/Header.js
@@ -7,15 +7,18 @@ import Carousel from '../components/carousel/Carousel';
 function Header() {
   return (
     <header className="Header">
-      <div className="-fixed-top">
+
+      <div className="fixed-top bg-color-main">
         <div className="logo-container">
-          <img src={logo} alt="Logo" className="logo"/>
+          <img src={logo} alt="Logo" className="logo" />
         </div>
         <Navbar />
         <hr />
-    </div>
-      
-      <Carousel />
+
+      </div>
+      <div className="clearfix p-t-230">
+        <Carousel />
+      </div>
     </header>
   );
 }
