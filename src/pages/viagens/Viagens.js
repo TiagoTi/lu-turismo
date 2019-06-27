69e1c2936031fcfa79bcf16779a9ab2c10796826
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-26 22:51:17 -0300

    create viagens page

---
+++ src/pages/viagens/Viagens.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default class Viagens extends React.Component{
+    render(){
+        return(
+            <div className="viagens">oi</div>
+        )
+    }
+}
