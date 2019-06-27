506a514abf430087ec90a0e414845193fe0dda43
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-06-26 22:50:20 -0300

    move about page to correct folder

---
+++ src/pages/about/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Footer from '../menu/Footer';
+import Header from '../menu/Header';
+import './About.sass'
+
+function About() {
+  return (
+    <div className="about container" id="about-us">
+      <Header />
+      <div className="height_100">
+      <h1>Quem Somos</h1>
+      <p>
+        <strong>Lu turismo</strong> é uma empresa que começou 25 de julho em 2001 em uma pequena sala, recebendo todos os seus clientes com muito carinho e realizando sonhos de sempre fazer viagens com o público em geral, onde <em>viajar é privilégio de todos</em>. Estamos a quase oito anos no mercado fazendo muitos momentos se tornarem <em className="blue">Inésqueciveis</em>.
+      </p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
